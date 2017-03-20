5f02005160ee1ee0173069ea7e2aaf2d6cc1bb88
jtegtmeier <jcbtegtmeier@gmail.com>
2017-03-20 11:02:14 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import base from 're-base'
+import './App.css'
+
+var authHandler = function(error, user) {
+  if(error) {console.log(error)}
+  console.log("user: ", user)
+}
 
 class App extends Component {
+  componentDidMount() {
+    console.log
+    base.authWithPassword({
+      email: 'jcbtegtmeier@gmail.com',
+      password: 'reacttest042'
+    }, authHandler)
+  }
+
   render() {
     return (
       <div className="App">
+        <div className="create-user-box">
+          <input/>
+          <input/>
+          <input/>
+        </div>
+        <div className="login-box">
+          <input/>
+          <input/>
+        </div>
+        <div className="message-box">
+
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
